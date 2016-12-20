0521b2516860d01b823de46409b6f6a287f7761b
cheryly279 <cheryl.yaeger@gmail.com>
2016-12-20 11:39:42 -0500

    Getting url, changing app to extension

--- submit.js
+++ submit.js
@@ -2,20 +2,24 @@ function addTopic(e) {
     e.preventDefault();
     var topicName = document.getElementById('name').value;
 
-    fetch('http://ec2-54-144-194-47.compute-1.amazonaws.com:8081/topic', {
-    	method: 'POST',
-        body: JSON.stringify({
-            addedBy: 1,
-    		name: document.getElementById('name').value,
-    		description: document.getElementById('description').value
-    	}),
-    	headers: new Headers({
-    		'Content-Type': 'application/json'
-    	})
-    }).then(function(result) {
-        document.getElementById('status').innerHTML = 'Topic ' + topicName + ' added successfully!<br /><br />';
-    }).catch(function(err) {
-        document.getElementById('status').innerHTML = 'Error: ' + err.message + '<br /><br />';
+    chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+        var url = tabs[0].url;
+
+        fetch('http://ec2-54-144-194-47.compute-1.amazonaws.com:8081/topic', {
+        	method: 'POST',
+            body: JSON.stringify({
+                addedBy: 1,
+        		name: document.getElementById('name').value,
+        		description: document.getElementById('description').value + ", \r\nurl: " + url
+        	}),
+        	headers: new Headers({
+        		'Content-Type': 'application/json'
+        	})
+        }).then(function(result) {
+            document.getElementById('status').innerHTML = 'Topic ' + topicName + ' added successfully!<br /><br />';
+        }).catch(function(err) {
+            document.getElementById('status').innerHTML = 'Error: ' + err.message + '<br /><br />';
+        });
     });
 }
 
